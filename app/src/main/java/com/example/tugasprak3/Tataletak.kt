6b4028784bfacbd612b99d398381c1f709ce23bc
Aulianurfitria15 <aulianurfitria17@gmail.com>
2025-10-17 16:59:11 +0700

    memperbaiki fun TataletakBoxColumnRow

--- app/src/main/java/com/example/tugasprak3/Tataletak.kt
+++ app/src/main/java/com/example/tugasprak3/Tataletak.kt
@@ -123,3 +123,24 @@ fun TataletakBoxColumnRow(modifier: Modifier) {
             }
         }
     }
+    Spacer(modifier = Modifier.height(10.dp))
+    Box(
+        modifier = modifier
+            .fillMaxWidth()
+            .height(300.dp)
+            .background(color = Color.Cyan),
+        contentAlignment = Alignment.Center
+    ){
+        Image(painter = gambar,
+            contentDescription = null,
+            contentScale = ContentScale.Fit)
+        Text(text = "My Team",
+            fontSize = 50.sp,
+            color = Color.Red,
+            fontWeight = FontWeight.Bold,
+            fontFamily = FontFamily.Cursive,
+            modifier= Modifier.align(
+                alignment = Alignment.BottomCenter
+            ))
+    }
+}
